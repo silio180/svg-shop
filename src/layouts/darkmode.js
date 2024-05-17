697520c48ab82fe2ba08573994cd143d12186937
silio180 <ivan16scr7@gmail.com>
2024-05-17 23:50:52 +0200

    ADD A NEW DARKMODE PRESET

--- src/layouts/darkmode.js
+++ src/layouts/darkmode.js
@@ -1,7 +1,14 @@
+    document.addEventListener('DOMContentLoaded', () => {
+    const checkbox = document.getElementById('checkbox');
 
-const checkbox =
-document.getElementById('checkbox');
+    const isLightMode = localStorage.getItem('isLightMode') === 'true';
+    if (isLightMode) {
+    document.body.classList.add('light');
+    checkbox.checked = true;
+    }
 
-checkbox.addEventListener('change', () =>{
-document.body.classList.toggle('light');
+    checkbox.addEventListener('change', () =>{
+    document.body.classList.toggle('light');
+    localStorage.setItem('isLightMode', checkbox.checked);
+    });
 });
