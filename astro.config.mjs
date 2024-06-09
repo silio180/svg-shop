4a0929d0c0a1533a0fd9e8f026963ade4ec02bb2
silio180 <ivan16scr7@gmail.com>
2024-06-09 22:26:53 +0200

    -.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [auth()],
-  output: "server",
-  adapter: netlify(),
+  output: 'server',
+  adapter: netlify({
+    edgeMiddleware: true
+  }),
 });
